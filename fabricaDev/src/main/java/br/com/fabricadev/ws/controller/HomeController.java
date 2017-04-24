2d9475614d75858051643718434cdae786a90651
GiovaneOliveiraTI <giovaneoliveira_ti@hotmail.com>
2017-04-24 09:24:50 -0300

    consumindo serviços com Divisão de responsabilidades

--- fabricaDev/src/main/java/br/com/fabricadev/ws/controller/HomeController.java
+++ fabricaDev/src/main/java/br/com/fabricadev/ws/controller/HomeController.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	@RequestMapping("/home")
+	
+	@RequestMapping("/")
 	public String irParaHome(){
 		return "index";
 		
--- fabricaDev/src/main/resources/static/js/app.js
+++ fabricaDev/src/main/resources/static/js/app.js
@@ -1,3 +1,9 @@
 //Criação do Modulo principal da Aplicação
-var appCliente = angular.module("appCliente", []);
+var appCliente = angular.module("appCliente", ['ngRoute']);
 
+appCliente.config(function($routeProvider) {
+
+	$routeProvider.when("/clientes", {templateUrl:'view/cliente.html' , controller:'clienteController'})
+	.otherwise({rediretTo:'/' });
+
+});
